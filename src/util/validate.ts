30552cce70eab11496144a7e03746594f8d53c4d
HyungJu <sungkisa@naver.com>
2024-03-16 13:31:28 +0900

    fix: validateBody에서 입력받은 스키마 인스턴스로 사용

--- src/util/validate.ts
+++ src/util/validate.ts
@@ -1,13 +1,12 @@
 import {validate} from "class-validator";
 import {plainToInstance} from "class-transformer";
 import {NextFunction, Request, Response} from "express";
-import {MyVaccinationRequest} from "../dto/my-vaccination";
 import {ErrorResponse} from "../dto/error";
 import {ErrorCode} from "../types/error";
 
 export function validateBody(schema: { new(): any }) {
     return async function (req: Request, res: Response, next: NextFunction) {
-        const dto = plainToInstance(MyVaccinationRequest, req.body)
+        const dto = plainToInstance(schema, req.body)
 
         const errors = await validate(dto, {
             whitelist: true,
