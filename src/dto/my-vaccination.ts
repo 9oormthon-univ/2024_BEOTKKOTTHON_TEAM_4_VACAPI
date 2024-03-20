7c323809fabbb907e4a9a2360dd5077664fa2cb4
HyungJu <sungkisa@naver.com>
2024-03-20 21:50:10 +0900

    feat: 백신 접종 내역 response 튜닝

--- src/dto/my-vaccination.ts
+++ src/dto/my-vaccination.ts
@@ -22,39 +22,54 @@ export class DetailData {
 }
 
 export class VaccineData {
-  epidemicType!: string
-  epidemicName!: string
+  vaccineType!: string
+  order!: number
+  orderString!: string
+  date!: number
+  agency!: string
   vaccineName!: string
-  inoculationOrder!: string
-  detailList!: DetailData[]
+  vaccineBrandName!: string
+  lotNumber!: string
 }
 
 export class MyVaccinationResponse {
   name!: string
+  birth!: string
+  sex!: string
   vaccineList!: VaccineData[]
 
   static fromCodefResponse (response: CodefMyVaccinationResponse): MyVaccinationResponse {
+    const sexIdentityNo = response.data.resUserIdentiyNo.slice(7, 8)
+    const sex = sexIdentityNo === '1' || sexIdentityNo === '3' ? 'M' : 'F'
+
+    const vaccinations = response.data.resVaccineList.filter(
+      (vaccine) => {
+        return vaccine.resDetailList.length > 0
+      }
+    ).flatMap(
+      (vaccine) => {
+        return vaccine.resDetailList.map(
+          (detail): VaccineData => {
+            return {
+              vaccineType: vaccine.resVaccineNm,
+              order: parseInt(vaccine.resInoculationOrder),
+              orderString: detail.resInoculationOrder,
+              date: parseInt(detail.resInoculationDate),
+              agency: detail.resInoculationAgency,
+              vaccineName: detail.resVaccineNm,
+              vaccineBrandName: detail.commBrandName,
+              lotNumber: detail.resLOTNumber
+            }
+          }
+        )
+      }
+    )
+
     return {
       name: response.data.resUserNm,
-      vaccineList: response.data.resVaccineList.map(
-        (vaccine): VaccineData => ({
-          epidemicType: vaccine.resEpidemicType,
-          epidemicName: vaccine.resEpidemicNm,
-          vaccineName: vaccine.resVaccineNm,
-          inoculationOrder: vaccine.resInoculationOrder,
-          detailList: vaccine.resDetailList.map(
-            (detail): DetailData => ({
-              vaccinationName: detail.resVaccinationNm,
-              inoculationOrder: detail.resInoculationOrder,
-              inoculationDate: detail.resInoculationDate,
-              inoculationAgency: detail.resInoculationAgency,
-              vaccineName: detail.resVaccineNm,
-              commBrandName: detail.commBrandName,
-              lotNumber: detail.resLOTNumber
-            })
-          )
-        }
-        ))
+      birth: response.data.resUserIdentiyNo.slice(0, 6),
+      sex,
+      vaccineList: vaccinations
     }
   }
 }
